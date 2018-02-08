cafadc298d444bd1f9e7c8e6d853fe92387d9cc9
Ayush <abansal1008@gmail.com>
2018-02-08 11:17:53 +0530

    updated utils.h

--- Assignment3_UDP/utils.h
+++ Assignment3_UDP/utils.h
@@ -1,7 +1,15 @@
 #define BUFF_SIZE 1024
+#define MAX_FILE_NAME 30
+#define MAX_SEQ_NO 65535
+
 struct segment{
 	int seqNo;
 	int len;
 	char data[BUFF_SIZE];
+};
+
+struct file_details{
+    char filename[MAX_FILE_NAME];
+    int filesize;
 };
 
