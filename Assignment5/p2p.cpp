0e31b2d5a482c8740e00be292cfb30c42afbfb3b
Ayush <abansal1008@gmail.com>
2018-02-26 14:29:40 +0000

    WIP, sending msg left

--- Assignment5/p2p.cpp
+++ Assignment5/p2p.cpp
@@ -1,13 +1,20 @@
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <string.h>
+#include <unistd.h>
+#include <iostream>
 #include <fstream>
 #include <map>
+#include <vector>
+#include <fcntl.h>
+#include <string>
 
 #define MAX_NAME_LEN 20
+#define MAX_BUF_SIZE 1024
+#define S second
+
 using namespace std;
 
 
@@ -41,13 +48,27 @@ map<string, struct sockaddr_in> get_user_info(){
 	}
 
 	return res;
-	
-
 }
+
+/*map<string, int> init_isOpen(){
+	map<string, int> ans;
+	ifstream f;
+	f.open("user_info.in");
+	string name;
+	while(f>>name)
+	{
+		ans[name] = 0;
+		char hostname[MAX_NAME_LEN];
+		f>>hostname;
+		int portno;
+		f>>portno;
+	}
+	return ans;
+}*/
 
 int main(int argc, char ** argv)
 {
-	int sockfd;
+	int server_sock;
 
 	if(argc != 2)
 	{
@@ -57,17 +78,14 @@ int main(int argc, char ** argv)
 	int portno = atoi(argv[1]);
 
 	map<string, struct sockaddr_in> user_info = get_user_info();
-	/*map<string, struct sockaddr_in>::iterator itr;
-	for(itr = user_info.begin();itr != user_info.end();itr++)
-	{
-		cout<<itr->first<<endl;
-	}*/
+	//map<string, int> isOpen = init_isOpen();
+	vector<int> open_peers(5,0);
 
-	sockfd = socket(AF_INET,SOCK_STREAM,0);
-	if(sockfd < 0) cout<<"Error opening socket"<<endl;
+	server_sock = socket(AF_INET,SOCK_STREAM,0);
+	if(server_sock < 0) cout<<"Error opening socket"<<endl;
 
 	int optval  = 1;
-	setsockopt(sockfd, SOL_SOCKET,SO_REUSEADDR,(const void *) &optval, sizeof(int));
+	setsockopt(server_sock, SOL_SOCKET,SO_REUSEADDR,(const void *) &optval, sizeof(int));
 
 	struct sockaddr_in serveraddr;
 	bzero((char *) &serveraddr, sizeof(serveraddr));
@@ -76,8 +94,118 @@ int main(int argc, char ** argv)
 	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serveraddr.sin_port = htons((unsigned short) portno);
 
-	if(bind(sockfd, (struct sockaddr *) &serveraddr,sizeof(serveraddr)) < 0)
+	if(bind(server_sock, (struct sockaddr *) &serveraddr,sizeof(serveraddr)) < 0)
 		cout<<"Error binding Server Socket to port"<<endl;
+
+	if(listen(server_sock, 5) < 0){
+		cout<<"Error listening"<<endl;
+	}
+
+	fd_set readfds;
+
+	int max_sd = server_sock;
+
+	struct sockaddr_in clientaddr;
+	socklen_t clientlen;
+
+	char buffer[MAX_BUF_SIZE];
+
+	while(1)
+	{
+		FD_ZERO(&readfds);
+		FD_SET(server_sock, &readfds);
+		FD_SET(0,&readfds);
+		/*map<string, int>::iterator itr;
+		for(itr = isOpen.begin(); itr != isOpen.end();itr++)
+		{
+			int sd = itr->S;
+			if(sd > 0)
+			{
+				FD_SET(sd, &readfds);
+				max_sd = max(sd,max_sd);
+			}
+		}*/
+		for(int i=0;i<5;i++)
+		{
+			if(open_peers[i] > 0)
+			{
+				FD_SET(open_peers[i],&readfds);
+				max_sd = max(open_peers[i], max_sd);
+			}
+		}
+
+		int activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+		if(activity < 0)
+		{
+			cout<<"Select error"<<endl;
+		}
+		if(FD_ISSET(0,&readfds))
+		{
+			//stdin
+			bzero(buffer,MAX_BUF_SIZE);
+			int n = read(0,buffer, MAX_BUF_SIZE);
+			if(n==0)
+			{
+				cout<<"Error reading from stdin"<<endl;
+				continue;
+			}
+			string ip = string(buffer);
+			size_t found = ip.find("/");
+			//cout<<"User: "<<ip.substr(0,found)<<" Meaasge: "<<ip.substr(found+1)<<endl;
+			string username = ip.substr(0,found);
+			cout<<"Username: "<<username<<endl;
+			strcpy(buffer, ip.substr(found+1).c_str());
+			map<string,struct sockaddr_in>::iterator itr = user_info.find(username);
+			if(itr == user_info.end())
+			{
+				cout<<"Invalid Username"<<endl;
+			}
+			else
+			{
+				cout<<"User exists"<<endl;
+				//TO CONTINUE
+			}
+
+		}
+
+		if(FD_ISSET(server_sock,&readfds))
+		{
+			int childfd = accept(server_sock, (struct sockaddr *) &clientaddr, &clientlen);
+			if(childfd < 0)
+			{
+				cout<<"Error in accept"<<endl;
+				continue;
+			}
+			for(int i=0;i<5;i++)
+			{
+				if(open_peers[i]==0)
+				{
+					open_peers[i] = childfd;
+					cout<<"Peer added to list"<<endl;
+				}
+			}
+
+		}
+
+		bzero(buffer, MAX_BUF_SIZE);
+		for(int i=0;i<5;i++)
+		{
+			if(FD_ISSET(open_peers[i],&readfds))
+			{
+				int n = read(open_peers[i], buffer, MAX_BUF_SIZE);
+				if(n==0)
+				{
+					cout<<"Disconnecting"<<endl;
+					close(open_peers[i]);
+					open_peers[i] = 0;
+				}
+				cout<<buffer<<endl;
+				bzero(buffer,MAX_BUF_SIZE);
+			}
+		}
+		
+
+	}
 
 
 
